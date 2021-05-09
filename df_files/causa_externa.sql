430d50d4fdc2dc066df406a3e11ad9a13dd27231
Julian Saldarriaga Blandon <Jsaldarriagab@unal.edu.co>
2021-05-09 11:16:42 -0500

    actualizar

---
+++ df_files/causa_externa.sql
@@ -0,0 +1,17 @@
+INSERT INTO causa_externa('cod','causa_externa')
+VALUES
+(1,'Accidente de trabajo'),
+(2,'Accidente de tránsito'),
+(3,'Accidente rábico'),
+(4,'Accidente ofídico'),
+(5,'Otro tipo de accidente'),
+(6,'Evento catastrófico'),
+(7,'Lesión por agresión'),
+(8,'Lesión auto inflingida'),
+(9,'Sospecha de maltrato físico'),
+(10,'Sospecha de abuso sexual'),
+(11,'Sospecha de violencia sexual'),
+(12,'Sospecha de maltrato emocional'),
+(13,'Enfermedad general'),
+(14,'Enfermedad profesional'),
+(15,'Otra');
